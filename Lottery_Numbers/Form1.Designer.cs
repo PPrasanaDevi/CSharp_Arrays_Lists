e2883040c81b10456bf2cd919854f47c8743ac23
Prasana Devi Palanisamy <prasanadevip@gmail.com>
2021-01-12 18:00:39 -0600

    random numbers added to the array.

--- Lottery_Numbers/Form1.Designer.cs
+++ Lottery_Numbers/Form1.Designer.cs
@@ -29,13 +29,98 @@ namespace Lottery_Numbers
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.btnGNumbers = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(32, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(70, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(118, 39);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(65, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(201, 39);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(67, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(284, 39);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(72, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(390, 39);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(72, 20);
+            this.textBox5.TabIndex = 4;
+            // 
+            // btnGNumbers
+            // 
+            this.btnGNumbers.Location = new System.Drawing.Point(108, 114);
+            this.btnGNumbers.Name = "btnGNumbers";
+            this.btnGNumbers.Size = new System.Drawing.Size(122, 23);
+            this.btnGNumbers.TabIndex = 5;
+            this.btnGNumbers.Text = "Generate Numbers";
+            this.btnGNumbers.UseVisualStyleBackColor = true;
+            this.btnGNumbers.Click += new System.EventHandler(this.btnGNumbers_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(284, 114);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(530, 209);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnGNumbers);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Random Lottery Numbers";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button btnGNumbers;
+        private System.Windows.Forms.Button btnExit;
     }
 }
 
--- Lottery_Numbers/Form1.cs
+++ Lottery_Numbers/Form1.cs
@@ -16,5 +16,33 @@ namespace Lottery_Numbers
         {
             InitializeComponent();
         }
+
+        private void btnGNumbers_Click(object sender, EventArgs e)
+        {
+            //array to hold random numbers.
+            const int SIZE = 5;
+            int[] lotteryNumbers = new int[SIZE];
+
+            //generate random numbers.
+            Random rand = new Random();
+
+            //fill in the array with random numbers ranges from 0 to 99.
+
+            for(int i =0; i < lotteryNumbers.Length; i++)
+            {
+                lotteryNumbers[i] = rand.Next(100);
+            }
+            //display the arrays.
+            textBox1.Text = lotteryNumbers[0].ToString();
+            textBox2.Text = lotteryNumbers[1].ToString();
+            textBox3.Text = lotteryNumbers[2].ToString();
+            textBox4.Text = lotteryNumbers[3].ToString();
+            textBox5.Text = lotteryNumbers[4].ToString();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
